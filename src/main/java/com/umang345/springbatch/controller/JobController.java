f5a9d33d2ec534bce3a37af9f2fd07985cea182b
Umang <umangagarwal345@gmail.com>
2022-06-28 09:52:14 +0530

    Added Post Rest Endpoint to transfer data from CSV to database

---
+++ src/main/java/com/umang345/springbatch/controller/JobController.java
@@ -0,0 +1,51 @@
+package com.umang345.springbatch.controller;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    /***
+     * Injecting JobLauncher Dependency
+     */
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    /***
+     * Injecting the Job Bean from Config class
+     */
+    @Autowired
+    private Job job;
+
+    /***
+     * Endpoint to initiate data transfer from csv to Database
+     */
+    @PostMapping("/importCustomers")
+    public void importCsvToDBJob() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        } catch (JobRestartException e) {
+            e.printStackTrace();
+        } catch (JobInstanceAlreadyCompleteException e) {
+            e.printStackTrace();
+        } catch (JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+}
